a94fb9987d15c7ccfce7b3a31f91837f416ce3cc
rav263 <nickiforov.nik@gmail.com>
2018-02-22 23:12:14 +0300

    Добавлено решение задачи А на ассемблере

--- A.asm
+++ A.asm
@@ -1,7 +1,40 @@
-%include "io.inc"
+%include 'io.inc'
 
 section .bss
+  v resd 2
+  a resd 2
+  t resd 1
   
 section .text
+global CMAIN
+CMAIN:
+  GET_DEC 4, [v]
+  GET_DEC 4, [v + 4]
+  GET_DEC 4, [a]
+  GET_DEC 4, [a + 4]
+  GET_DEC 4, [t]
 
+  MOV EAX, DWORD [v]
+  IMUL EAX, DWORD [t]
+  
+  MOV EBX, DWORD [a]
+  IMUL EBX, DWORD [t]
+  IMUL EBX, DWORD [t]
 
+  ADD EAX, EBX
+  PRINT_DEC 4, EAX
+  NEWLINE
+
+  MOV EAX, DWORD [v + 4]
+  IMUL EAX, DWORD [t]
+
+  MOV EBX, DWORD[a + 4]
+  IMUL EBX, DWORD[t]
+  IMUL EBX, DWORD[t]
+  
+  ADD EAX, EBX
+  PRINT_DEC 4, EAX
+  NEWLINE
+
+  MOV EAX, 0
+  RET
