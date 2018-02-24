47956968553646665c6f823faaf42c7bff25bf5b
rav263 <nickiforov.nik@gmail.com>
2018-02-24 15:02:33 +0300

    Решена задача D

--- D.asm
+++ D.asm
@@ -1,10 +1,31 @@
 %include 'io.inc'
 
 section .bss
-
+  x resb 1
+  y resb 1
 
 section .text
 global CMAIN
 CMAIN:
-  
+  GET_CHAR [x]
+  GET_CHAR [y]
+
+  SUB BYTE[x], 'H'
+  SUB BYTE[y], '8'
+
+  MOVSX EAX, BYTE[x]
+  MOVSX EBX, BYTE[y]
+
+  IMUL EAX, EBX
+
+  SHR EAX, 1
+
+  PRINT_DEC 4, EAX
+  NEWLINE
+
+
+  MOV EAX, 0
+
+  RET
+
 
